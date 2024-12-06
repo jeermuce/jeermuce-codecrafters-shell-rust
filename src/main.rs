52fd56e2111439ef23647c1305791aa2fb842333
jeermuce <ernesto_mc.dev@proton.me>
2024-12-06 16:49:34 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::SplitWhitespace;
 
 fn main() {
     // Wait for user input
@@ -9,10 +10,19 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         let _read = stdin.read_line(&mut input).unwrap();
-
-        match input.trim() {
-            "exit 0" => {
-                std::process::exit(0);
+        let mut arr: SplitWhitespace<'_> = input.split_whitespace();
+        match arr.next().unwrap() {
+            "exit" => match arr.next().unwrap_or_default() {
+                "0" => {
+                    std::process::exit(0);
+                }
+                other => {
+                    println!("unknown argument: {}", other);
+                }
+            },
+            "echo" => {
+                let string: String = arr.collect::<Vec<&str>>().join(" ");
+                println!("{string}");
             }
             _ => {
                 println!("{}: command not found", input.trim());
